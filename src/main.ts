caf0e603a2e51e953ee8e23a64c4129f3e39d2c5
SinoLewis <sinolewis@gmail.com>
2022-10-08 01:49:45 +0300

    Main app init

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import InstantSearch from 'vue-instantsearch/vue3/es';
+import App from './App.vue'
+import router from './router'
+
+import './style.css'
+
+const app = createApp(App)
+
+app.use(InstantSearch)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
