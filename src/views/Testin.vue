7bdbfab407f990231a2a5e5c3a836c907e81cce5
SinoLewis <sinolewis@gmail.com>
2022-11-03 04:25:55 +0300

    Init Pagination compo.

--- src/views/Testin.vue
+++ src/views/Testin.vue
@@ -1,69 +1,24 @@
 <template>
-    <div class="grid place-content-evenly">
-        <div class="w-full min-w-max px-2 sm:px-0">
-            <TabGroup>
-                <TabList class="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-                    <Tab v-for="category in checkouts" as="template" :key="category" v-slot="{ selected }">
-                        <button :class="[
-                            'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
-                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                            selected
-                                ? 'bg-white shadow'
-                                : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
-                        ]">
-                            <div class="flex gap-2 justify-center items-center">
-                                <!-- TODO: Replace with Prop compo; Perfomance Test  check on for if loops-->
-                                <ShoppingCartIcon class="w-8 h-8" v-if="category === 'Cart'" />
-                                <TruckIcon class="w-8 h-8" v-if="category === 'Delivery'" />
-                                <CurrencyDollarIcon class="w-8 h-8" v-if="category === 'Payments'" />
-                                <ComputerDesktopIcon class="w-8 h-8" v-if="category === 'Order'" />
-                                <span class="p-2">{{ category }}</span>
-                            </div>
-                        </button>
-                    </Tab>
-                </TabList>
-                <TabPanels class="mt-2">
-                    <TabPanel :class="[
-                        'rounded-xl p-3',
-                        'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                    ]">
-                        <Cart />
-                    </TabPanel>
-                    <TabPanel :class="[
-                        'rounded-xl p-3',
-                        'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                    ]">
-                        <Delivery />
-                    </TabPanel>
-                    <TabPanel :class="[
-                        'rounded-xl p-3',
-                        'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                    ]">
-                        <Payment />
-                    </TabPanel>
-                    <TabPanel :class="[
-                        'rounded-xl p-3',
-                        'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                    ]">
-                        <Order />
-                    </TabPanel>
-                </TabPanels>
-            </TabGroup>
-        </div>
-    </div>
+    <pagination :totalPages="10" :perPage="10" :currentPage="currentPage" @pagechanged="onPageChange" />
 </template>
   
-<script setup lang="ts">
-import { ref } from 'vue'
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
-import { HeartIcon, ShoppingCartIcon, TruckIcon, CurrencyDollarIcon, ComputerDesktopIcon } from '@heroicons/vue/20/solid'
-import Cart from "@/components/checkout/Cart.vue";
-import Delivery from "@/components/checkout/Delivery.vue";
-import Payment from "@/components/checkout/Payment.vue";
-import Order from "@/components/checkout/Order.vue";
-import type { Product } from "@/types/product";
-import { useShopStore } from '@/stores/shop';
+<script lang="ts">
+import Pagination from '@/components/Pagination.vue'
 
-const { inCart, addCartProduct, removeCartProduct, removeCartProducts } = useShopStore();
-const checkouts = ['Cart', 'Delivery', 'Payments', 'Order']
+export default {
+    components: {
+        Pagination
+    },
+    data() {
+        return {
+            currentPage: 1,
+        };
+    },
+    methods: {
+        onPageChange(page) {
+            console.log(page)
+            this.currentPage = page;
+        }
+    }
+}
 </script>
