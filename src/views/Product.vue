f0882f445aa57978eea2a1b1c94c8f1c179eadda
SinoLewis <sinolewis@gmail.com>
2022-11-05 00:18:22 +0300

    Btn & Badge themes.

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -7,27 +7,31 @@
             <h2 class="card-title">{{ shop.current_product.title }}</h2>
             <p>{{ shop.current_product.description }}</p>
             <div class="flex gap-1">
-                <div class="badge badge-lg badge-info font-bold">Ksh {{ shop.current_product.price }}
+                <div class="badge badge-lg bg-secondary-focus font-bold">Ksh <span class="px-1 text-black">
+                        {{ shop.current_product.price }}
+                    </span>
                 </div>
                 <!-- <div class="badge badge-lg badge-accent font-bold">Stock: {{ product.quantity }}</div> -->
-                <div class="badge badge-lg badge-accent font-bold">Cart: {{ cartItems(shop.current_product.id) }}</div>
+                <div class="badge badge-lg bg-accent-focus font-bold">In Cart <span class="px-1 text-black">
+                        {{ cartItems(shop.current_product.id) }}
+                    </span></div>
             </div>
             <div class="card-actions justify-end">
                 <button class="btn btn-sm btn-outline btn-accent">
                     <HeartIcon class="h-5 w-5" />
                 </button>
                 <button v-if="!inCart(shop.current_product)" @click="addCartProduct(shop.current_product)"
-                    class="btn btn-sm btn-outline btn-info">
+                    class="btn btn-sm btn-outline btn-secondary">
                     <ShoppingCartIcon class="h-5 w-5" />
                 </button>
                 <div v-else class="grid grid-cols-3 gap-1">
-                    <button @click="addCartProduct(shop.current_product)" class="btn btn-xs btn-accent">
+                    <button @click="addCartProduct(shop.current_product)" class="btn btn-xs bg-secondary-focus">
                         <PlusIcon class="h-5 w-5" />
                     </button>
-                    <button @click="removeCartProduct(shop.current_product)" class="btn btn-xs btn-error">
+                    <button @click="removeCartProduct(shop.current_product)" class="btn btn-xs bg-accent-focus">
                         <MinusIcon class="h-5 w-5" />
                     </button>
-                    <button @click="removeCartProducts(shop.current_product)" class="btn btn-sm btn-error">
+                    <button @click="removeCartProducts(shop.current_product)" class="btn btn-sm bg-accent-focus">
                         <TrashIcon class="h-5 w-5" />
                     </button>
                 </div>
