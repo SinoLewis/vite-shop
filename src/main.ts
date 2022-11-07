c47149cd65495fdbf2626be3d15749b4c255bd2c
SinoLewis <sinolewis@gmail.com>
2022-11-07 03:24:35 +0300

    PWA init & Notification lib install.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 // import InstantSearch from 'vue-instantsearch';
 import App from './App.vue'
 import router from './router'
-
 import './style.css'
 
 const app = createApp(App)
@@ -13,9 +12,3 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('service worker registered', reg))
-        .catch(err => console.log('service worker not registered', err));
-}
--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,53 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    // VitePWA({
-    //   // Register SW
-    //   registerType: 'autoUpdate',
-    //   // Dev mode
-    //   devOptions: {
-    //     enabled: true
-    //   },
-    //   // Precache
-    //   workbox: {
-    //     globPatterns: [
-    //       // './index.html',
-    //       // '**/*.{js,vue,json,css,html,ico,png,svg}'
-    //     ]
-    //   },
-    // })
+    VitePWA({
+      // Register SW
+      registerType: 'autoUpdate',
+      // Dev mode
+      devOptions: {
+        enabled: true
+      },
+      // Precache
+      workbox: {
+        globPatterns: [
+          // './index.html',
+          '**/*.{js,vue,json,css,html,ico,png,svg}'
+        ]
+      },
+      // manifest
+      manifest: {
+        "name": "Lee Shop",
+        "short_name": "LeeShop",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#3b82f6",
+        "theme_color": "#1e40af",
+        "orientation": "portrait-primary",
+        "icons": [
+          {
+            "src": "/src/assets/icons/icon-16x16.png",
+            "type": "image/png",
+            "sizes": "16x16"
+          },
+          {
+            "src": "/src/assets/icons/icon-72x72.png",
+            "type": "image/png",
+            "sizes": "72x72"
+          },
+          {
+            "src": "/src/assets/icons/icon-192x192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "/src/assets/icons/icon-512x512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
