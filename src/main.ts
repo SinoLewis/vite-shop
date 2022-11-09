4292ddd4456e041cec14a0ffdc102db12c5c5fd6
SinoLewis <sinolewis@gmail.com>
2022-11-10 00:31:40 +0300

    Add Instantsearch & notification css.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-// import InstantSearch from 'vue-instantsearch';
+import InstantSearch from 'vue-instantsearch/vue3/es';
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import 'vue-toast-notification/dist/theme-sugar.css';
 
 const app = createApp(App)
 
-// app.use(InstantSearch)
+app.use(InstantSearch)
 app.use(createPinia())
 app.use(router)
 
