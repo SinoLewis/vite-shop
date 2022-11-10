e8e732fc80eeef88da102fc4b07b1a2bcf6e6231
SinoLewis <sinolewis@gmail.com>
2022-11-10 15:53:35 +0300

    Init Products index for meilisearch.

--- server/meilisearch.ts
+++ server/meilisearch.ts
@@ -5,16 +5,18 @@ try {
   // Note that jsonString will be a <Buffer> since we did not specify an
   // encoding type for the file. But it'll still work because JSON.parse() will
   // use <Buffer>.toString().
-  const jsonString = fs.readFileSync("./movies.json");
-  const movies = JSON.parse(jsonString);
+  // const jsonString = fs.readFileSync("./movies.json");
+  // const movies = JSON.parse(jsonString);
+  const jsonString = fs.readFileSync("./products.json");
+  const products = JSON.parse(jsonString);
 
   // 1. Init client SDK
   const client = new MeiliSearch({ host: "http://localhost:7700" });
 
   // // 2. Add docs & check server response
   client
-    .index("movies")
-    .addDocuments(movies)
+    .index("products")
+    .addDocuments(products)
     .then((res) => console.log(res));
 
   // // 3. Check the status
